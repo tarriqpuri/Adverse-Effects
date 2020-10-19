afe29f9e10901602d1bfdc92b4f4874d253d62f3
TarriqPuri <Tarriq.Purivatra@mail.utoronto.ca>
2020-10-19 17:09:28 -0400

    Cleaned up spark job code

--- src/spark/main.py
+++ src/spark/main.py
@@ -1,45 +1,17 @@
-"""
-This app loads data from s3 and dumps the cleaned/manipulated
-data into a postgresql database
-"""                                                                                                                                                                                                                                                                                                                                                                                         tables.py                                                                                                                                                                                                                                                                                                                                                                                                       
+'''
+MAIN.PY: Read 800 JSON files from S3 bucket, clean data, transform into
+Patients, Drugs, and Reactions tables, write to PostgreSQL
+'''
 
-
-import sys
-from operator import add
-
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as sf
-
-from pyspark.sql.types import StructType, ArrayType
-from pyspark.sql.window import Window
-from pyspark.sql.functions import rank, dense_rank
-
-def flatten(schema, prefix=None):
-    fields = []
-    for field in schema.fields:
-        name = prefix + '.' + field.name if prefix else field.name
-        dtype = field.dataType
-        if isinstance(dtype, ArrayType):
-            dtype = dtype.elementType
-
-        if isinstance(dtype, StructType):
-            fields += flatten(dtype, prefix=name)
-        else:
-            fields.append(name)
-
-    return fields
+from main_functions import *
 
 import os
 os.environ["PYSPARK_PYTHON"] = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7"
 os.environ["PYSPARK_DRIVER_PYTHON"] = "/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7"
 
 if __name__ == "__main__":
-        """
-        if len(sys.argv) != 2:
-                print("Usage: wordcount <file>", file=sys.stderr)
-                sys.exit(-1)
-        """
+
+        #INITIALIZE SPARK SESSION
         spark = SparkSession\
                 .builder\
                 .appName("s3 to spark to postgres")\
@@ -47,131 +19,48 @@ if __name__ == "__main__":
         sc = spark.sparkContext
         sc.setLogLevel("ERROR")
 
-        #Set Parameters for Spark Job
-        spark.conf.set("spark.sql.shuffle.partitions", 50)
-        #spark.conf.set("spark.sql.files.maxPartitionBytes", 16777216)
-        spark.conf.set("spark.sql.files.maxPartitionBytes", 32777216)
-        #s3url="s3a://tarriq-spark-test/adverse-effects/drug-event-0001-of-0025.json"
-        s3url="s3a://tarriq-spark-test/adverse-effects/subset/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/2016-Q4-drug-event-0001-of-0023.json/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/2014-Q3-drug-event-0001-of-0017.json/*"
-
-        POSTGRESQL_URL = ""
-        POSTGRESQL_TABLE = ""
+        #POSTGRES CREDENTIALS
+        POSTGRESQL_URL = "jdbc:postgresql://ec2-34-229-140-20.compute-1.amazonaws.com:5431/test"
+        POSTGRESQL_TABLE = "substance"
         POSTGRESQL_USER = ""
         POSTGRESQL_PASSWORD = ""
 
 
-        df = spark.read.option("multiLine","true").json(s3url)
-        print(df.rdd.getNumPartitions())
-        #df.cache()
-        #df.show()
-        #df.printSchema()
+        #SET PARAMETERS FOR SPARK JOB
+        num = 60
+        spark.conf.set("spark.sql.shuffle.partitions", num)
+        spark.conf.set("spark.sql.files.maxPartitionBytes", 16777216)
 
-        """
-        #CLEAN DF (DELETE USELESS COLUMNS)
-        df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.*","results.patient.*")
-        df=df.drop("authoritynumb")
-        df=df.drop("fulfilledexpeditecriteria")
-        df=df.drop("patient.summary")
-        df=df.drop("sender")
-        df.printSchema()
-        """
+        for x in range(4,21):
+                if x<10:
+                        year = '200' + str(x)
+                else:
+                        year = '20' + str(x)
 
-
-        #PATIENT DF
-        patient_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.*","results.patient.*")
-        patient_df=patient_df.drop("patient")
-        patient_df=patient_df.drop("drug")
-        patient_df=patient_df.drop("reaction")
-        patient_df=patient_df.select(flatten(patient_df.schema))
-        #patient_df.show()
-        #patient_df.printSchema()
+                #S3 BUCKET
+                s3url="s3a://tarriq-spark-test/adverse-effects/*"
+                
+                #READ DATAFRAME
+                df = spark.read.option("multiLine","true").json(s3url)
 
 
-        #DRUG DF
-        drug_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.safetyreportid", \
-        "results.patient.drug")
-        #drug_df.show()
-        drug_df=drug_df.withColumn("drug",sf.explode(sf.col("drug")))
-        drug_df=drug_df.select(flatten(drug_df.schema))
-        #drug_df.show()
-        #drug_df.printSchema()
-        #avoid arrays
-        drug_df=drug_df.withColumn("generic_name",drug_df["generic_name"].getItem(0))\
-        .withColumn("application_number",drug_df["application_number"].getItem(0))\
-        .withColumn("brand_name",drug_df["brand_name"].getItem(0))\
-        .withColumn("manufacturer_name",drug_df["manufacturer_name"].getItem(0))\
-        .withColumn("nui",drug_df["nui"].getItem(0))\
-        .withColumn("package_ndc",drug_df["package_ndc"].getItem(0))\
-        .withColumn("pharm_class_cs",drug_df["pharm_class_cs"].getItem(0))\
-        .withColumn("pharm_class_epc",drug_df["pharm_class_epc"].getItem(0))\
-        .withColumn("pharm_class_moa",drug_df["pharm_class_moa"].getItem(0))\
-        .withColumn("product_ndc",drug_df["product_ndc"].getItem(0))\
-        .withColumn("product_type",drug_df["product_type"].getItem(0))\
-        .withColumn("route",drug_df["route"].getItem(0))\
-        .withColumn("rxcui",drug_df["rxcui"].getItem(0))\
-        .withColumn("spl_id",drug_df["spl_id"].getItem(0))\
-        .withColumn("spl_set_id",drug_df["spl_set_id"].getItem(0))\
-        .withColumn("substance_name",drug_df["substance_name"].getItem(0))\
-        .withColumn("unii",drug_df["unii"].getItem(0))
-        #drug_df.show()
-        window = Window.partitionBy(drug_df['safetyreportid']).orderBy(drug_df["brand_name"])
-        drug_df=drug_df.select('*', rank().over(window).alias('index'))
-        drug_df=drug_df.withColumn("drug-key",sf.concat(sf.col('safetyreportid'),sf.lit('-'),sf.col('index')))
-        #drug_df.show()
-        #print("Drug df length")
-        #print(drug_df.select("safetyreportid").distinct().count())
-
-
-        #Reaction DF
-        reaction_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.safetyreportid", \
-        "results.patient.reaction")
-        #reaction_df.show()
-        reaction_df=reaction_df.withColumn("reaction",sf.explode(sf.col("reaction")))
-        reaction_df=reaction_df.select(flatten(reaction_df.schema))
-        #reaction_df.show()
-        window = Window.partitionBy(reaction_df['safetyreportid']).orderBy(reaction_df["reactionmeddrapt"])
-        reaction_df=reaction_df.select('*', rank().over(window).alias('index'))
-        reaction_df=reaction_df.withColumn("reaction-key",sf.concat(sf.col('safetyreportid'),sf.lit('-'),sf.col('index')))
-        reaction_df.show()
-        #print("Reaction df length")
-        #print(reaction_df.select("safetyreportid").distinct().count())
-
-
-        patient_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "patients") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-        drug_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "drugs") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-        reaction_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "reactions") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-
+                #CREATE TABLES
+                patient_df = create_patient_df(df)
+                bad_df, drug_df = create_drug_df(df)
+                reaction_df = create_reaction_df(df)
+                
+                #WRITE TO POSTGRES DATABASE
+                if year == '2004':                        
+                    postgres_write(patient_df, drug_df, reaction_df, bad_df, \
+                        POSTGRESQL_URL, POSTGRESQL_USER, POSTGRESQL_PASSWORD)
+                else:
+                     postgres_append(patient_df, drug_df, reaction_df, bad_df, \
+                         POSTGRESQL_URL, POSTGRESQL_USER, POSTGRESQL_PASSWORD)
         spark.stop()
+
+
+
+
 
 
 
--- src/spark/main_functions.py
+++ src/spark/main_functions.py
@@ -113,139 +113,84 @@ def create_reaction_df (df):
     reaction_df=reaction_df.withColumn("reaction-key",sf.concat(sf.col('safetyreportid'),sf.lit('-'),sf.col('index')))
     return reaction_df
 
-def postgres_write(patient_df, drug_df, reaction_df, bad_df):
-    
+def postgres_write(patient_df, drug_df, reaction_df, bad_df, POSTGRESQL_URL, POSTGRESQL_USER, POSTGRESQL_PASSWORD):
+    patient_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "patients") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("overwrite") \
+            .save()
 
+    drug_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "drugs") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("overwrite") \
+            .save()
 
-        #Set Parameters for Spark Job
-        spark.conf.set("spark.sql.shuffle.partitions", 50)
-        #spark.conf.set("spark.sql.files.maxPartitionBytes", 16777216)
-        spark.conf.set("spark.sql.files.maxPartitionBytes", 32777216)
-        #s3url="s3a://tarriq-spark-test/adverse-effects/drug-event-0001-of-0025.json"
-        s3url="s3a://tarriq-spark-test/adverse-effects/subset/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/2016-Q4-drug-event-0001-of-0023.json/*"
-        #s3url="s3a://tarriq-spark-test/adverse-effects/2014-Q3-drug-event-0001-of-0017.json/*"
+    reaction_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "reactions") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("overwrite") \
+            .save()
 
-        POSTGRESQL_URL = "jdbc:postgresql://ec2-34-229-140-20.compute-1.amazonaws.com:5431/test"
-        POSTGRESQL_TABLE = "substance"
-        POSTGRESQL_USER = "tarriq"
-        POSTGRESQL_PASSWORD = "insight"
+    bad_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "badCases") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("overwrite") \
+            .save()
 
+def postgres_append(patient_df, drug_df, reaction_df, bad_df, POSTGRESQL_URL, POSTGRESQL_USER, POSTGRESQL_PASSWORD):
+    patient_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "patients") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("append") \
+            .save()
 
-        df = spark.read.option("multiLine","true").json(s3url)
-        print(df.rdd.getNumPartitions())
-        #df.cache()
-        #df.show()
-        #df.printSchema()
+    drug_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "drugs") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("append") \
+            .save()
 
+    reaction_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "reactions") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("append") \
+            .save()
 
-        #CLEAN DF (DELETE USELESS COLUMNS)
-        df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.*","results.patient.*")
-        df=df.drop("authoritynumb")
-        df=df.drop("fulfilledexpeditecriteria")
-        df=df.drop("patient.summary")
-        df=df.drop("sender")
-        df.printSchema()
-
-
-
-        #PATIENT DF
-        patient_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.*","results.patient.*")
-        patient_df=patient_df.drop("patient")
-        patient_df=patient_df.drop("drug")
-        patient_df=patient_df.drop("reaction")
-        patient_df=patient_df.select(flatten(patient_df.schema))
-        #patient_df.show()
-        #patient_df.printSchema()
-
-
-        #DRUG DF
-        drug_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.safetyreportid", \
-        "results.patient.drug")
-        #drug_df.show()
-        drug_df=drug_df.withColumn("drug",sf.explode(sf.col("drug")))
-        drug_df=drug_df.select(flatten(drug_df.schema))
-        #drug_df.show()
-        #drug_df.printSchema()
-        #avoid arrays
-        drug_df=drug_df.withColumn("generic_name",drug_df["generic_name"].getItem(0))\
-        .withColumn("application_number",drug_df["application_number"].getItem(0))\
-        .withColumn("brand_name",drug_df["brand_name"].getItem(0))\
-        .withColumn("manufacturer_name",drug_df["manufacturer_name"].getItem(0))\
-        .withColumn("nui",drug_df["nui"].getItem(0))\
-        .withColumn("package_ndc",drug_df["package_ndc"].getItem(0))\
-        .withColumn("pharm_class_cs",drug_df["pharm_class_cs"].getItem(0))\
-        .withColumn("pharm_class_epc",drug_df["pharm_class_epc"].getItem(0))\
-        .withColumn("pharm_class_moa",drug_df["pharm_class_moa"].getItem(0))\
-        .withColumn("product_ndc",drug_df["product_ndc"].getItem(0))\
-        .withColumn("product_type",drug_df["product_type"].getItem(0))\
-        .withColumn("route",drug_df["route"].getItem(0))\
-        .withColumn("rxcui",drug_df["rxcui"].getItem(0))\
-        .withColumn("spl_id",drug_df["spl_id"].getItem(0))\
-        .withColumn("spl_set_id",drug_df["spl_set_id"].getItem(0))\
-        .withColumn("substance_name",drug_df["substance_name"].getItem(0))\
-        .withColumn("unii",drug_df["unii"].getItem(0))
-        #drug_df.show()
-        window = Window.partitionBy(drug_df['safetyreportid']).orderBy(drug_df["brand_name"])
-        drug_df=drug_df.select('*', rank().over(window).alias('index'))
-        drug_df=drug_df.withColumn("drug-key",sf.concat(sf.col('safetyreportid'),sf.lit('-'),sf.col('index')))
-        #drug_df.show()
-        #print("Drug df length")
-        #print(drug_df.select("safetyreportid").distinct().count())
-
-
-        #Reaction DF
-        reaction_df=df.withColumn("results",sf.explode(sf.col("results"))).select("results.safetyreportid", \
-        "results.patient.reaction")
-        #reaction_df.show()
-        reaction_df=reaction_df.withColumn("reaction",sf.explode(sf.col("reaction")))
-        reaction_df=reaction_df.select(flatten(reaction_df.schema))
-        #reaction_df.show()
-        window = Window.partitionBy(reaction_df['safetyreportid']).orderBy(reaction_df["reactionmeddrapt"])
-        reaction_df=reaction_df.select('*', rank().over(window).alias('index'))
-        reaction_df=reaction_df.withColumn("reaction-key",sf.concat(sf.col('safetyreportid'),sf.lit('-'),sf.col('index')))
-        reaction_df.show()
-        #print("Reaction df length")
-        #print(reaction_df.select("safetyreportid").distinct().count())
-
-
-        patient_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "patients") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-        drug_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "drugs") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-        reaction_df.write \
-                .format("jdbc") \
-                .option("url", POSTGRESQL_URL) \
-                .option("dbtable", "reactions") \
-                .option("user", POSTGRESQL_USER) \
-                .option("password", POSTGRESQL_PASSWORD) \
-                .option("driver","org.postgresql.Driver")\
-                .mode("overwrite") \
-                .save()
-
-
-
-        spark.stop()
-
-
-
-
-
-
+    bad_df.write \
+            .format("jdbc") \
+            .option("url", POSTGRESQL_URL) \
+            .option("dbtable", "badCases") \
+            .option("user", POSTGRESQL_USER) \
+            .option("password", POSTGRESQL_PASSWORD) \
+            .option("driver","org.postgresql.Driver")\
+            .mode("append") \
+            .save()
